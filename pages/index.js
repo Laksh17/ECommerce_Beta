6a083ca54104931ca44db2cb29377bc758cd6fdf
Ninad <ninad1064@gmail.com>
2022-07-28 19:41:31 +0530

    connected metamask

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,49 @@
-import React from 'react'
-import { client } from '../lib/client'
-import { Product, FooterBanner, HeroBanner } from '../components'
-import { useWeb3 } from '../context/StateContext'
-// import { useWeb3 } from '../components/providers/web3'
+import React, { useState, useEffect } from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
+import { useWeb3 } from "../context/StateContext";
 
 const Home = ({ products, bannerData }) => {
-  const _web3api  = useWeb3()
-  const { web3, provider, connect }  = useWeb3()
-  console.log(connect)
+  const { web3, provider } = useWeb3();
+  const [account, setAccount] = useState(null);
+
+  provider && provider.on("accountsChanged", () => window.location.reload());
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3 && getAccount();
+  }, [web3]);
+
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      <div className='products-heading'>
-        <h2>
-          Best Selling Products
-        </h2>
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
         <p>Speakers that please one and all!</p>
       </div>
-     {
-      provider && <button onClick={connect}>connect </button>
-     }
-      <div className='products-container'>
-        {products?.map((product) => <Product key={product._id} product={product} />)}
+      {account}
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-
       <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async () => {
-  const query = '*[_type == "product"]'
-  const products = await client.fetch(query)
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
 
-  const bannerQuery = '*[_type == "banner"]'
-  const bannerData = await client.fetch(bannerQuery)
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
 
   return {
-    props: { products, bannerData }
-  }
-}
-export default Home
+    props: { products, bannerData },
+  };
+};
+export default Home;
